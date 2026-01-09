4efb9c5f86252b36c29df3c3187815811d47afe2
mrbi10 <hetmyerwi@gmail.com>
2026-01-09 23:03:52 +0530

    Timetable change included

--- server.js
+++ server.js
@@ -22,16 +22,31 @@ const groq = new Groq({
 // =======================
 // MySQL Connection Pool
 // =======================
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_NAME || 'mnmjec_erp',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mnmjec_erp',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0
-});
+  queueLimit: 0,
 
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 
 
 
