805c0dc7be94950b00c501cd10df7ea7b5461f5a
mrbi10 <hetmyerwi@gmail.com>
2026-01-18 11:28:07 +0530

    1801

--- server.js
+++ server.js
@@ -22,32 +22,32 @@ const groq = new Groq({
 // =======================
 // MySQL Connection Pool
 // =======================
-const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'mnmjec_erp',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
-  timezone: 'Z'
-});
-
 // const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT || 3306,
-
+//   host: process.env.DB_HOST || 'localhost',
+//   user: process.env.DB_USER || 'root',
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_NAME || 'mnmjec_erp',
 //   waitForConnections: true,
 //   connectionLimit: 10,
 //   queueLimit: 0,
-
-//   ssl: {
-//     rejectUnauthorized: true
-//   }
+//   timezone: 'Z'
 // });
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  ssl: {
+    rejectUnauthorized: true
+  }
+});
 
 const submitAttemptInternal = async (attemptId) => {
   const conn = await pool.getConnection();
